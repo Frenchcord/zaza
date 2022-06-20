c007ba623987b2ea824bdc69a9165b2eb2fa0794
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-19 23:23:52 -0400

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .get import get_salon, get_channel, messageContext, get_serveur, get_guild, get_utilisateur, get_user, get_membre, get_member, get_message, get_role, get_emoji,  get_sticker, get_all_roles, get_all_members, get_all_channels
+from .message import Message
+from .context import Context
+from .guild import Guild
+from .role import Role
+from .stickers import Stickers
